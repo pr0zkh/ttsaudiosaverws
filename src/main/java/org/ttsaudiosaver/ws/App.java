aa378dc12ac9f181514950dec4b231ed6f7c5c53
Yurii Banin <pr0z.kh@gmail.com>
2015-11-02 18:47:43 +0200

    Initial WS commit.

---
+++ src/main/java/org/ttsaudiosaver/ws/App.java
@@ -0,0 +1,13 @@
+package org.ttsaudiosaver.ws;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
