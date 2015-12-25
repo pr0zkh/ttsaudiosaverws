83bcdabe021dfe32492da53ad05b486261cbe1cc
Yurii Banin <pr0z.kh@gmail.com>
2015-12-25 18:41:53 +0200

    Adding cross-domain resource handling

--- src/main/java/org/ttsaudiosaver/ws/config/WebAppInitializer.java
+++ src/main/java/org/ttsaudiosaver/ws/config/WebAppInitializer.java
@@ -1,5 +1,6 @@
 package org.ttsaudiosaver.ws.config;
 
+import javax.servlet.FilterRegistration;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
@@ -9,6 +10,7 @@ import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
+import org.ttsaudiosaver.ws.filter.CORSFilter;
 
 public class WebAppInitializer implements WebApplicationInitializer {
 	
@@ -20,6 +22,8 @@ public class WebAppInitializer implements WebApplicationInitializer {
 	public void onStartup(ServletContext servletContext) throws ServletException {
 		WebApplicationContext context = getContext();
 		servletContext.addListener(new ContextLoaderListener(context));
+		FilterRegistration.Dynamic corsFilter = servletContext.addFilter("corsFilter", CORSFilter.class);
+        corsFilter.addMappingForUrlPatterns(null, false, "/*");
 		ServletRegistration.Dynamic dispatcher = servletContext.addServlet(DISPATCHER_SERVLET_KEY, new DispatcherServlet(context));
 		dispatcher.setLoadOnStartup(1);
 		dispatcher.addMapping(DISPATCHER_SERVLET_MAPPING);
@@ -30,5 +34,4 @@ public class WebAppInitializer implements WebApplicationInitializer {
 		context.register(WebConfig.class);
 		return context;
 	}
-
 }
